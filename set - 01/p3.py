29082571d5c3ba00288c2c934e47195cc56ece1c
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2024-02-04 20:58:03 +0530

    Update p3.py

--- set - 01/p3.py
+++ set - 01/p3.py
@@ -1,16 +1,16 @@
 # Write a Python program to swap two variables without third variable
 
-var1 = 10
-var2 = 20
+var1 = float(input("Enter the value for 'Variable 1': "))
+var2 = float(input("\nEnter the value for 'Variable 2': "))
 
-print("Before swapping:")
-print("Variable 1:", var1)
-print("Variable 2:", var2)
+print("\nBefore swapping:")
+print("\nVariable 1:", var1)
+print("\nVariable 2:", var2)
 
 var1 = var1 + var2
 var2 = var1 - var2
 var1 = var1 - var2
 
-print("After swapping:")
-print("Variable 1:", var1)
-print("Variable 2:", var2)
+print("\nAfter swapping:")
+print("\nVariable 1:", var1)
+print("\nVariable 2:", var2)
