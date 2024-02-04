1bc91c50c11edb308c7c76fc3299ad2eb12d7a0a
pillar008 <sharmaneeraj4759@gmail.com>
2024-02-04 20:29:30 +0530

    set-1 p4 added

--- set - 01/p2.py
+++ set - 01/p2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Write a Python program to swap two variables using third variable. 
 
 var1 = input("Enter the value of first variable: ")
@@ -16,20 +15,3 @@ var2 = temp
 print("\nAfter swapping:")
 print("First variable =", var1)
 print("Second variable =", var2)
-=======
-# Write a Python program to swap two variables using third variable = 10
-var1 = 10
-var2 = 20
-
-print("Before swapping:")
-print("Variable 1:", var1)
-print("Variable 2:", var2)
-
-temp = var1
-var1 = var2
-var2 = temp
-
-print("After swapping:")
-print("Variable 1:", var1)
-print("Variable 2:", var2)
->>>>>>> 4b3b78e0dbd039d28622785adb782fdfeb048af5
---
+++ set - 01/p4.py
@@ -0,0 +1,10 @@
+# Write a Python program to find square root of positive number. 
+
+import math
+value=int(input("Enter a positve number:"))
+
+if(value>0):
+    square= math.sqrt(value)
+    print(f"The square root of {value} is {square}.")
+else:
+    print("Error: Please enter a positive number.")
