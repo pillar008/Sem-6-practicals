446b11d0d05759c1f3cd60072969172224f0f429
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2024-02-04 20:54:11 +0530

    Create p8.py

---
+++ set - 01/p8.py
@@ -0,0 +1,6 @@
+# Write a Python program to check output of modulo operator
+dividend = int(input("Enter the dividend: "))
+divisor = int(input("Enter the divisor: "))
+
+modulo_result = dividend % divisor
+print(f"The result of {dividend} % {divisor} is: {modulo_result}")
