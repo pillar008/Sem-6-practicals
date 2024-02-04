4b3b78e0dbd039d28622785adb782fdfeb048af5
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2024-02-04 20:23:00 +0530

    Add files via upload

---
+++ set - 01/p2.py
@@ -0,0 +1,15 @@
+# Write a Python program to swap two variables using third variable = 10
+var1 = 10
+var2 = 20
+
+print("Before swapping:")
+print("Variable 1:", var1)
+print("Variable 2:", var2)
+
+temp = var1
+var1 = var2
+var2 = temp
+
+print("After swapping:")
+print("Variable 1:", var1)
+print("Variable 2:", var2)
