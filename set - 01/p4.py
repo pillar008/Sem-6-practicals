8a0fbf804f8cecc8eb602ebe5a55de6d34f0d94c
pillar008 <sharmaneeraj4759@gmail.com>
2024-02-04 20:32:52 +0530

    changed

--- set - 01/p4.py
+++ set - 01/p4.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Write a Python program to find square root of positive number. 
 
 import math
@@ -9,15 +8,3 @@ if(value>0):
     print(f"The square root of {value} is {square}.")
 else:
     print("Error: Please enter a positive number.")
-=======
-# Write a Python program to find square root of positive number
-import math
-
-num = float(input("Enter a positive number: "))
-
-if num >= 0:
-    result = math.sqrt(num)
-    print(f"The square root of {num} is: {result}")
-else:
-    print("Cannot find square root of a negative number.")
->>>>>>> a0dd07bffcf69c77fcf3e744afa67677d405d50f
