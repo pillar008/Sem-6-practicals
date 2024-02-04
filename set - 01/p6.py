2e59b34d5f8532a73a2a474f8a6de9eb176522f6
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2024-02-04 20:53:19 +0530

    Update p6.py

--- set - 01/p6.py
+++ set - 01/p6.py
@@ -5,6 +5,6 @@ n = int(input("Enter a positive integer (n): "))
 sum = n * (n + 1) // 2
 
 if n > 0:
-    print(f"The sum of the first {n} natural numbers is: {sum}")
+    print("\nThe sum of the first {n} natural numbers is: {sum}")
 else:
     print("Please enter a positive integer.")
