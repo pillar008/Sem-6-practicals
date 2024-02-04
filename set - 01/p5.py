ced172ed07ad0d20829109bac09778b687cb065b
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2024-02-04 20:55:46 +0530

    Update p5.py

--- set - 01/p5.py
+++ set - 01/p5.py
@@ -2,16 +2,16 @@
 import math
 
 # Input values for rectangle
-rectangle_length = float(input("Enter the length of the rectangle: "))
-rectangle_width = float(input("Enter the width of the rectangle: "))
+rectangle_length = float(input("\nEnter the length of the rectangle: "))
+rectangle_width = float(input("\nEnter the width of the rectangle: "))
 
 # Calculate and print rectangle area
 rectangle_area = rectangle_length * rectangle_width
-print(f"The area of the rectangle is: {rectangle_area}")
+print("\nThe area of the rectangle is: {rectangle_area}")
 
 # Input value for circle
-circle_radius = float(input("Enter the radius of the circle: "))
+circle_radius = float(input("\nEnter the radius of the circle: "))
 
 # Calculate and print circle area
 circle_area = math.pi * circle_radius**2
-print(f"The area of the circle is: {circle_area}")
+print("\nThe area of the circle is: {circle_area}")
